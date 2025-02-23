6f18a2daa908587d434d24801e4976ab0524f6a0
Ijaj Mulla <161236228+ijaj-mulla@users.noreply.github.com>
2025-02-23 11:19:45 +0530

    Add files via upload

---
+++ Student_record.cpp
@@ -0,0 +1,229 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<conio.h>
+struct Student
+{
+    int rollnumber;
+    char name[100];
+    char phone[100];
+    float percentage;
+    char branch[100];
+    char DOB[100];
+    struct Student *next;
+
+}* head;
+void insert(int rollnumber, char* name, char* branch,char* phone, float
+            percentage)
+{
+    struct Student *
+
+    student = (struct Student *) malloc(sizeof(struct Student));
+    student->rollnumber = rollnumber;
+    strcpy(student->name, name);
+    strcpy(student->phone, phone);
+    strcpy(student->branch, branch);
+    student->percentage = percentage;
+    student->next = NULL;
+
+    if(head==NULL)
+    {
+// if head is NULL
+// set student as the new head
+        head = student;
+    }
+    else
+    {
+// if list is not empty
+// insert student in beginning of head
+        student->next = head;
+        head = student;
+    }
+
+    printf("\n\n Student Record Inserted succesfully !!!");
+}
+void search(int rollnumber)
+{
+    struct Student * temp = head;
+    while(temp!=NULL)
+    {
+        if(temp->rollnumber==rollnumber)
+        {
+            printf("\n");
+            printf(" Roll Number : %d\n", temp->rollnumber);
+            printf(" Name : %s\n", temp->name);
+            printf(" Branch : %s\n",temp->branch);
+            printf(" Phone : %s\n", temp->phone);
+            printf(" Percentage : %0.4f\n", temp->percentage);
+
+            return;
+        }
+        temp = temp->next;
+    }
+    printf(" Student with roll number %d is not found !!!\n", rollnumber);
+}
+void update(int rollnumber)
+{
+
+    struct Student * temp = head;
+    while(temp!=NULL)
+    {
+
+        if(temp->rollnumber==rollnumber)
+        {
+            printf("Record with roll number %d Found !!!\n", rollnumber);
+            printf("Enter new name: ");
+            scanf("%s", temp->name);
+            printf("Enter new phone number: ");
+            scanf("%s", temp->phone);
+            printf("Enter new Branch: ");
+            scanf("%s", temp->branch);
+            printf("Enter new percentage: ");
+            scanf("%f",&temp->percentage);
+            printf("Updation Successful!!!\n");
+            return;
+        }
+        temp = temp->next;
+
+    }
+    printf("Student with roll number %d is not found !!!\n", rollnumber);
+}
+void Delete(int rollnumber)
+{
+    struct Student * temp1 = head;
+    struct Student * temp2 = head;
+    while(temp1!=NULL)
+    {
+
+        if(temp1->rollnumber==rollnumber)
+        {
+
+            printf("Record with roll number : %d :Found !!!\n", rollnumber);
+            if(temp1==temp2)
+            {
+// this condition will run if
+// the record that we need to delete is the first node
+// of the linked list
+                head = head->next;
+                free(temp1);
+            }
+            else {
+// temp1 is the node we need to delete
+// temp2 is the node previous to temp1
+                temp2->next = temp1->next;
+                free(temp1);
+            }
+
+            printf("Record Deleted Successfully !!!\n");
+            return;
+
+        }
+        temp2 = temp1;
+        temp1 = temp1->next;
+
+    }
+    printf("Student with roll number %d is not found !!!\n", rollnumber);
+
+}
+void display()
+{
+    struct Student * temp = head;
+    while(temp!=NULL)
+    {
+
+        printf("\n\t %d",temp->rollnumber);
+        printf("\t %s ",temp->name);
+        printf("\t %s",temp->branch);
+        printf("\t %s",temp->phone);
+        printf("\t %0.4f",temp->percentage);
+        temp = temp->next;
+    }
+}
+int main()
+{
+    head = NULL;
+    int choice;
+    char name[100];
+    char phone[100];
+    char branch[100];
+    int rollnumber;
+    float percentage;
+    do
+    {
+        system("cls");
+        printf("\n\t\t\t\t\t***************************************");
+        printf("\n\t\t\t\t\t     * GPKP STUDENT RECORD MENU * ");
+        printf("\n\t\t\t\t\t***************************************");
+        printf("\n");
+        printf("\n\t\t\t\t\t 1. Insert Student RECORD");
+        printf("\n\t\t\t\t\t 2. Search Student RECORD");
+        printf("\n\t\t\t\t\t 3. Delete Student RECORD");
+        printf("\n\t\t\t\t\t 4. Update Student RECORD");
+        printf("\n\t\t\t\t\t 5. Display All student RECORD");
+        printf("\n\t\t\t\t\t 6. EXIT ");
+        printf("\n\t\t\t\t\t----------------------------------------");
+        printf("\n");
+        printf("\n \t\t\t\t\t = Enter Choice : ");
+
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            printf("\n Enter Student Details");
+            printf("\n **************************");
+            printf("\n");
+            printf("\n Enter roll number: ");
+            scanf("%d", &rollnumber);
+            printf(" Enter name: ");
+            scanf("%s", name);
+            printf(" Enter Branch name: ");
+            scanf("%s", branch);
+            printf(" Enter phone number: ");
+            scanf("%s", phone);
+            printf(" Enter percentage: ");
+            scanf("%f", &percentage);
+            insert(rollnumber, name, branch,phone, percentage);
+            break;
+        case 2:
+            printf("\n Search Student Details ");
+            printf("\n **************************");
+            printf("\n");
+            printf(" Enter roll number to search: ");
+            scanf("%d", &rollnumber);
+            search(rollnumber);
+            break;
+        case 3:
+            printf("\n Delete Student Details ");
+            printf("\n **************************");
+            printf("\n");
+            printf(" Enter roll number to delete: ");
+            scanf("%d", &rollnumber);
+            Delete(rollnumber);
+            break;
+        case 4:
+            printf("\n Update Student Details");
+            printf("\n **************************");
+            printf("\n");
+            printf(" Enter roll number to update: ");
+            scanf("%d", &rollnumber);
+            update(rollnumber);
+            break;
+        case 5:
+            printf("\n\t\t ****************************************");
+            printf("\n\t\t List of Student Data ");
+            printf("\n\t\t ***************************************");
+            printf("\n");
+            printf("\n\tRoll.NO\t Name \t branch\t phone\t percentage");
+            printf("\n\t---------------------------------------------------------");
+            display();
+            break;
+        case 6:
+            printf("\n\t\t\t\t\t THANKS FOR USING MY SOFTWERE !!!! BY BY BY ");
+                   break;
+               default:
+                       printf("\n\t\t\t\t\t Please Enter Valid choice ");
+            }
+        getch();
+    } while (choice !=6);
+}
+  
